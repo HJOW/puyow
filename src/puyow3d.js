07ec3ec96202c39caa1df5e63552a75687c7435c
HJOW <hujinone22@naver.com>
2026-08-28 01:08:44 +0900

    3D 버전 개발용 파일 미리 추가

---
+++ src/puyow3d.js
@@ -0,0 +1,19 @@
+/**
+ * @license Apache-2.0
+ * Copyright 2026 HJOW
+ * Licensed under the Apache License, Version 2.0.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * 뿌요 W 3D 버전 스크립트
+ *     의존성
+ *         three.core.min.js
+ *         puyow.css  (캔버스 영역이 화면 100%를 차지하게 만들고, 기본 뒷배경 색 변경)
+ *         notice.txt (선택사항으로 공지사항 존재 시 이 곳에 기재)
+ *     html 예제
+ *         puyow3d.html
+ */
+
+// TODO
+//     puyow.js 및 OLD_PROMPTS.md 내용을 참고하여, 3D 그래픽 버전으로 재개발
+//     Three.js 활용, CommonJS 형식으로 개발
+//     원근법 없이, 기존 2D 버전 게임처럼 플레이 영역이 모두 정면에서 보여야 함.
