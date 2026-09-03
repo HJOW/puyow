44511570507cb85279983fc49b7676bd7261f612
HJOW <hujinone22@naver.com>
2026-09-04 01:20:42 +0900

    pythonserver.py 짧은 이름 버전

---
+++ python/server.py
@@ -0,0 +1,9 @@
+# Puyo W Python 서버 사용 안내
+#
+# 이 파일은 pythonserver.py 를 대신 호출하는 역할을 합니다.
+# 프로젝트 루트 디렉토리에서 이 파일을 실행해 주세요.
+#
+# 예:
+#      python python/server.py
+#
+exec(open("python/pythonserver.py", "r", encoding="UTF-8").read())
