3b849bb4dfafd9905586ba90837cac1ae1cc9c81
HJOW <hjow@djict.co.kr>
2026-09-07 17:24:56 +0900

    번들에 Three.js, JSON5 포함

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const TerserPlugin = require('terser-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
-    "entry" : ["./src/js/puyow.js"],
+    "entry" : ["./src/js/puyow.js", "./src/js/three.min.js", "./src/js/json5.min.js"],
     "output" : {
         "path" : __dirname + "/src/bundle/",
         "filename" : "puyow.bundle.js",
